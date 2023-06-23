74e5a94cae2d6d1b5f2e2bbea7b7291ef474777c
Giovanni Pires <giovannipds@gmail.com>
2023-06-22 21:46:55 -0300

    avoid giving error if the elements are not present

--- scripts/tableless.js
+++ scripts/tableless.js
@@ -1,4 +1,6 @@
 const postTimeRead = document.querySelector('.tb-post-time-read');
+if (!postTimeRead) return
+
 // Clear the current reading time
 postTimeRead.innerHTML = ''
 // Position it in a similar position of the other script
@@ -7,12 +9,12 @@ postTimeRead.style.float = 'unset';
 const article = document.querySelector("article");
 
 // `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wourdCount = [...words].length;
-  const readingTime = Math.round(wourdCount / 200);
-  postTimeRead.textContent = `⏱️ ${readingTime} min read`;
-}
+if (!article) return
+
+const text = article.textContent;
+const wordMatchRegExp = /[^\s]+/g; // Regular expression
+const words = text.matchAll(wordMatchRegExp);
+// matchAll returns an iterator, convert to array to get word count
+const wourdCount = [...words].length;
+const readingTime = Math.round(wourdCount / 200);
+postTimeRead.textContent = `⏱️ ${readingTime} min read`;

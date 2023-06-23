f0c68dd2d51c9ea4c64066bb1397eab26e1e93b8
Giovanni Pires <giovannipds@gmail.com>
2023-06-22 21:49:49 -0300

    avoid illegal return statement

--- scripts/tableless.js
+++ scripts/tableless.js
@@ -1,20 +1,18 @@
 const postTimeRead = document.querySelector('.tb-post-time-read');
-if (!postTimeRead) return
-
-// Clear the current reading time
-postTimeRead.innerHTML = ''
-// Position it in a similar position of the other script
-postTimeRead.style.float = 'unset';
-
 const article = document.querySelector("article");
 
 // `document.querySelector` may return null if the selector doesn't match anything.
-if (!article) return
+if (postTimeRead && article) {
+  // Clear the current reading time
+  postTimeRead.innerHTML = ''
+  // Position it in a similar position of the other script
+  postTimeRead.style.float = 'unset';
 
-const text = article.textContent;
-const wordMatchRegExp = /[^\s]+/g; // Regular expression
-const words = text.matchAll(wordMatchRegExp);
-// matchAll returns an iterator, convert to array to get word count
-const wourdCount = [...words].length;
-const readingTime = Math.round(wourdCount / 200);
-postTimeRead.textContent = `⏱️ ${readingTime} min read`;
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wourdCount = [...words].length;
+  const readingTime = Math.round(wourdCount / 200);
+  postTimeRead.textContent = `⏱️ ${readingTime} min read`;
+}
